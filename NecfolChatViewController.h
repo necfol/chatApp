59c9fc4a582cb855e36150e97536d6b0c824e197
necfol <necfoll@gmail.com>
2017-09-15 17:17:51 +0800

    init

---
+++ NecfolChatViewController.h
@@ -0,0 +1,13 @@
+//
+//  NecfolChatViewController.h
+//  chatApp
+//
+//  Created by necfol on 2017/9/15.
+//  Copyright © 2017年 Necfol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NecfolChatViewController : UIViewController
+
+@end
